58772754048a713459574d2e2f1f66b542bda588
wonseok <nsk7640@naver.com>
2021-09-29 11:46:33 +0900

    주석수정

--- src/main/java/com/nh/controller/BoardController.java
+++ src/main/java/com/nh/controller/BoardController.java
@@ -30,7 +30,7 @@ public class BoardController {
 
     @GetMapping("/board/list")
     public String getBoardList(Model model , @RequestParam(value = "currentPage" , defaultValue = "0") int currentPage) {
-        int pageData = 5;//한 페이지당 보여질 데이터 개수 (이건 내가 그냥 막 정하면되용)
+        int pageData = 5;//한 페이지당 보여질 데이터 개수 (이건 내가 그냥 막 정하면됩니당)
         int offset = currentPage*pageData; //전체 데이터(totalCount)에서 몇번째부터 시작점으로 잡을지 정할때 필요(예를들어 1번 페이지는 0번데이터부터  , 2번페이지는 5번데이터부터)_
         int totalCount = boardService.totalCount();
         model.addAttribute("currentPage",currentPage); //현재 페이지(화면에서 페이지버튼 눌린표시랑 안눌린표시 , 이전버튼 , 다음버튼 활성화 비활성화 할때 필요)
